8a6dac4582b6926533ce34377e776a3030e953e6
rcanbinici <canbinici.55@gmail.com>
2024-06-26 00:16:54 +0300

    first commit

---
+++ src/test/java/com/rcanbinici/spring/boot/aws/api/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcanbinici.spring.boot.aws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
